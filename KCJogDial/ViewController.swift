9344a95d92edb6cbcb14c6b968ee4157276c4a54
Lee Sun-Hyoup <kciter@naver.com>
2015-09-25 23:35:50 +0900

    Initial Commit

---
+++ KCJogDial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KCJogDial
+//
+//  Created by LeeSunhyoup on 2015. 9. 25..
+//  Copyright © 2015년 LeeSunhyoup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
