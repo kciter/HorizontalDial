c014d9ce92fe6acf6b5c2504aa88670a92ecc75f
Lee Sun-Hyoup <kciter@naver.com>
2015-09-26 19:24:17 +0900

    Added Sample and podspec

---
+++ KCJogDial.podspec
@@ -0,0 +1,13 @@
+Pod::Spec.new do |s|
+  s.name         = "KCJogDial"
+  s.version      = "1.0.0"
+  s.summary      = "It is controllable UIView like Jog Dial"
+  s.homepage     = "https://github.com/kciter/KCJogDial"
+  s.license      = { :type => 'MIT', :file => 'LICENSE' }
+  s.author       = { "kciter" => "kciter@naver.com" }
+  s.source       = { :git => "https://github.com/kciter/KCJogDial.git", :tag => s.version }
+  s.platform     = :ios, '8.0'
+  s.source_files = 'KCJogDial/*.{swift}'
+  s.frameworks   = 'UIKit', 'Foundation', 'QuartzCore'
+  s.requires_arc = true
+end
---
+++ Sample/KCJogDial/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  KCJogDial
+//
+//  Created by LeeSunhyoup on 2015. 9. 25..
+//  Copyright © 2015년 LeeSunhyoup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate, KCJogDialDelegate {
+    @IBOutlet var imageView: UIImageView?
+    @IBOutlet var jogDial: KCJogDial?
+    @IBOutlet var degreesValueLabel: UILabel?
+    @IBOutlet var tickLabel: UILabel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        jogDial?.delegate = self
+    }
+    
+    func jogDialDidValueChanged(jogDial: KCJogDial) {
+        let degrees = jogDial.value
+        let radians = degreesToRadians(degrees)
+        degreesValueLabel?.text = "\(round(degrees*100)/100) Degrees"
+        imageView?.transform = CGAffineTransformMakeRotation(CGFloat(radians))
+    }
+    
+    @IBAction func reset() {
+        jogDial?.animateWithValueUpdate(0, duration: 2)
+    }
+    
+    @IBAction func toMaximumValue() {
+        guard let maximumValue = jogDial?.maximumValue else { return }
+        jogDial?.animateWithValueUpdate(maximumValue, duration: 2)
+    }
+    
+    @IBAction func toMinimumValue() {
+        guard let minimumValue = jogDial?.minimumValue else { return }
+        jogDial?.animateWithValueUpdate(minimumValue, duration: 2)
+    }
+    
+    @IBAction func enableRangeValueChanged(_switch: UISwitch) {
+        jogDial?.enableRange = _switch.on
+    }
+    
+    @IBAction func tickValueChanged(stepper: UIStepper) {
+        tickLabel?.text = "Tick \(stepper.value)"
+        jogDial?.tick = stepper.value
+    }
+    
+    @IBAction func verticalAlignValueChanged(segmentControl: UISegmentedControl) {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            jogDial?.verticalAlign = "top"
+        case 1:
+            jogDial?.verticalAlign = "middle"
+        case 2:
+            jogDial?.verticalAlign = "bottom"
+        default:
+            return
+        }
+    }
+    
+    /// private function ///
+    private func degreesToRadians(degrees: Double) -> Double {
+        return degrees*M_PI/180.0
+    }
+}
+
